61a88f69cba218ca9454fc3265ed31cb2c040541
static <TheStaticVoid@users.noreply.github.com>
2023-09-02 22:29:34 -0400

    warning fix

--- src/main/java/dev/thestaticvoid/mi_sound_addon/compat/kubejs/MISoundAddonKubeJSPlugin.java
+++ src/main/java/dev/thestaticvoid/mi_sound_addon/compat/kubejs/MISoundAddonKubeJSPlugin.java
@@ -2,15 +2,10 @@ package dev.thestaticvoid.mi_sound_addon.compat.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.thestaticvoid.mi_sound_addon.compat.kubejs.sound.MISoundKubeJSEvents;
-import dev.thestaticvoid.mi_sound_addon.compat.kubejs.sound.ModifySoundEventsJS;
 
 public class MISoundAddonKubeJSPlugin extends KubeJSPlugin {
     @Override
     public void registerEvents() {
         MISoundKubeJSEvents.EVENT_GROUP.register();
-    }
-
-    @Override
-    public void initStartup() {
     }
 }
