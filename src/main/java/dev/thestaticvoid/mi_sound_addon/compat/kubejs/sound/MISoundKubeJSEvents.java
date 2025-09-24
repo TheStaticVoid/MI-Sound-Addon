27e1b8fa83bfa01e1051166802b1ffa37e453a83
static <TheStaticVoid@users.noreply.github.com>
2025-09-24 10:32:36 -0400

    Fix EI issue

--- src/main/java/dev/thestaticvoid/mi_sound_addon/compat/kubejs/sound/MISoundKubeJSEvents.java
+++ src/main/java/dev/thestaticvoid/mi_sound_addon/compat/kubejs/sound/MISoundKubeJSEvents.java
@@ -4,7 +4,7 @@ import dev.latvian.mods.kubejs.event.EventGroup;
 import dev.latvian.mods.kubejs.event.EventHandler;
 
 public interface MISoundKubeJSEvents {
-    EventGroup EVENT_GROUP = EventGroup.of("MISoundAddons");
+    EventGroup EVENT_GROUP = EventGroup.of("MISoundAddons"); //xd why is this plural
 
     EventHandler MODIFY_SOUNDS = EVENT_GROUP.startup("modifySounds", () -> ModifySoundEventsJS.class);
 }
