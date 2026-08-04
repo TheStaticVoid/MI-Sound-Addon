e58fe5d58b803b6331ebd66808662ad3894167eb
static <TheStaticVoid@users.noreply.github.com>
2026-08-04 13:56:12 -0400

    Tesla tower sounds originate from the top of the multi

--- src/main/java/dev/thestaticvoid/mi_sound_addon/mixin/compat/extended_industrialization/TeslaTowerBlockEntityMixin.java
+++ src/main/java/dev/thestaticvoid/mi_sound_addon/mixin/compat/extended_industrialization/TeslaTowerBlockEntityMixin.java
@@ -1,12 +1,17 @@
 package dev.thestaticvoid.mi_sound_addon.mixin.compat.extended_industrialization;
 
 import aztech.modern_industrialization.machines.MachineBlockEntity;
+import aztech.modern_industrialization.machines.components.OrientationComponent;
 import com.llamalad7.mixinextras.sugar.Local;
+import dev.thestaticvoid.mi_sound_addon.MISoundAddon;
 import dev.thestaticvoid.mi_sound_addon.MISoundAddonConfig;
 import dev.thestaticvoid.mi_sound_addon.sound.ModSoundEventInfo;
 import dev.thestaticvoid.mi_sound_addon.sound.ModSounds;
 import dev.thestaticvoid.mi_sound_addon.util.SilencedComponent;
 import dev.thestaticvoid.mi_sound_addon.util.SilencedComponentInterface;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundSource;
 import net.swedz.extended_industrialization.EI;
 import net.swedz.extended_industrialization.machines.blockentity.multiblock.teslatower.TeslaTowerBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,8 +40,39 @@ public abstract class TeslaTowerBlockEntityMixin {
 
             if (currentGameTime > mI_Sound_Addon$lastSoundTime + teslaSoundEvent.getSoundDuration()) {
                 mI_Sound_Addon$lastSoundTime = currentGameTime;
-                ModSounds.playSoundNoRecipe(blockEntity, EI.id("tesla_tower"));
+                // ModSounds.playSoundNoRecipe(blockEntity, EI.id("tesla_tower"));
+
+                BlockPos newPos = mI_Sound_Addon$calculateTowerBlockPos(blockEntity.getBlockPos(), blockEntity.orientation);
+                MISoundAddon.LOGGER.debug(String.valueOf(newPos));
+                blockEntity.getLevel().playSound(null, newPos, teslaSoundEvent.getSoundEvent().get(),
+                        SoundSource.BLOCKS, teslaSoundEvent.getVolume(), 1.0F);
             }
         }
+    }
+
+    @Unique
+    private BlockPos mI_Sound_Addon$calculateTowerBlockPos(BlockPos blockPos, OrientationComponent orientation) {
+        final int vertical_offset = 14;
+        final int horizontal_offset = 3;
+        int x = blockPos.getX();
+        int y = blockPos.getY() + vertical_offset;
+        int z = blockPos.getZ();
+
+        switch (orientation.facingDirection) {
+            case Direction.NORTH:
+                z += horizontal_offset;
+                break;
+            case Direction.SOUTH:
+                z -= horizontal_offset;
+                break;
+            case Direction.EAST:
+                x -= horizontal_offset;
+                break;
+            case Direction.WEST:
+                x += horizontal_offset;
+                break;
+        }
+
+        return new BlockPos(x, y, z);
     }
 }
